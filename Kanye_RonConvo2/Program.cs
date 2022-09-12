3387fb38f028f90f2902dced855dd9655d75ee46
jess <jcottrill1123@gmail.com>
2022-09-12 16:31:26 -0400

    complete convo

---
+++ Kanye_RonConvo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+namespace Kanye_RonConvo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                var quote = new QuoteGenerator();
+                Console.WriteLine($"-----------------");
+                Console.WriteLine($"Kanye: '{QuoteGenerator.Kanye()}'");
+                Console.WriteLine($"-----------------");
+                Console.WriteLine($"Ron: '{QuoteGenerator.RonSwanson()}'");
+            }
+        }
+    }
+}
---
+++ Kanye_RonConvo2/QuoteGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanye_RonConvo
+
+{
+    public class QuoteGenerator
+    {
+
+        public static string Kanye()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public static string RonSwanson()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
+    }
+}
